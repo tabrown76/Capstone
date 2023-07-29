3f97c17565ab68b368f526fb0f37a8120f68b066
Timothy Brown <tabrown76@gmail.com>
2023-07-29 15:15:06 -0400

    -bug fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_login import LoginManager, login_required, current_user, logout_user, login_user
 from models import db, connect_db, User, Story, StoryStep, Choice, Genre, Character, UserGenre
 from forms import AddUserForm, LoginForm, EditUserForm, GenreForm, CharacterForm, EditStoryForm
-import config
+
 from apicalls import make_api_request, next_step
 from sqlalchemy import func
 from werkzeug.exceptions import HTTPException
@@ -22,40 +22,40 @@ login_manager.init_app(app)
 
 connect_db(app)
 
-# @app.errorhandler(Exception)
-# def handle_exception(e):
-#     """
-#     Handle general exceptions.
+@app.errorhandler(Exception)
+def handle_exception(e):
+    """
+    Handle general exceptions.
 
-#     This function handles all exceptions and returns the HTTPException if it is an instance of HTTPException.
-#     Otherwise, it redirects to the 'unhandled_exception' route. This is a part of the Flask error handling system.
+    This function handles all exceptions and returns the HTTPException if it is an instance of HTTPException.
+    Otherwise, it redirects to the 'unhandled_exception' route. This is a part of the Flask error handling system.
     
-#     Args:
-#         e (Exception): The exception that was raised.
+    Args:
+        e (Exception): The exception that was raised.
 
-#     Returns:
-#         HTTPException or Werkzeug Response: Returns the HTTPException if e is an instance of it. Otherwise, 
-#         it returns a redirection to 'unhandled_exception' route.
-#     """
+    Returns:
+        HTTPException or Werkzeug Response: Returns the HTTPException if e is an instance of it. Otherwise, 
+        it returns a redirection to 'unhandled_exception' route.
+    """
 
-#     if isinstance(e, HTTPException):
-#         return e
+    if isinstance(e, HTTPException):
+        return e
     
-#     return redirect(url_for('unhandled_exception'))
+    return redirect(url_for('unhandled_exception'))
 
-# @app.route('/oops')
-# def unhandled_exception():
-#     """
-#     Handle unhandled exceptions.
+@app.route('/oops')
+def unhandled_exception():
+    """
+    Handle unhandled exceptions.
 
-#     This function is used to handle all exceptions that are not explicitly caught elsewhere in the application.
-#     It renders the 'oops.html' template, which is typically used to display an error message to the user.
+    This function is used to handle all exceptions that are not explicitly caught elsewhere in the application.
+    It renders the 'oops.html' template, which is typically used to display an error message to the user.
 
-#     Returns:
-#         Rendered template (str): Returns the 'oops.html' template which is used for displaying the error message.
-#     """
+    Returns:
+        Rendered template (str): Returns the 'oops.html' template which is used for displaying the error message.
+    """
 
-#     return render_template('oops.html')
+    return render_template('oops.html')
 
 
 @login_manager.user_loader
